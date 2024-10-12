862420a875a829bd8001d0c675b63e3ef174e2cd
Haseeb175 <mrhaseeb200@gmail.com>
2024-10-12 22:59:07 +0500

    Add Create Student API

--- controller/studentController.js
+++ controller/studentController.js
@@ -24,6 +24,74 @@ const getAllStudentController = async (req, res) => {
             error
         })
     }
+};
+
+// Get Student by ID Controller
+const getStudentByIdController = async (req, res) => {
+    try {
+        const studentId = req.params.id;
+        if (!studentId) {
+            return res.status(404).json({
+                success: false,
+                message: "Student ID is missing OR Invalid ID"
+            })
+        }
+        const studentData = await DB.query(` SELECT * FROM students WHERE id=?`, [studentId]);
+        if (!studentData) {
+            return res.status(404).json({
+                success: false,
+                message: " NO Student Record Found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "Student Record Found Sucessfully",
+            studentData: studentData[0]
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error in Get Student By ID API",
+            error
+        })
+    }
+};
+
+// Create Student Controller
+const createStudentController = async (req, res) => {
+    try {
+
+        const { name, roll_no, fees, classs, medium } = req.body;
+
+        if (!name || !roll_no || !fees || !classs || !medium) {
+            return res.status(404).json({
+                success: false,
+                message: " Please Provide All fields"
+            })
+        }
+        const createStudentData = DB.query(` INSERT INTO students (name, roll_no, fees, classs, medium) VALUES (? , ? , ? , ? , ?)`, [name, roll_no, fees, classs, medium]);
+
+        if (!createStudentData) {
+            return res.status(404).json({
+                success: false,
+                message: " Error in INSERT Query"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Student Data INSERT Successfully"
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error in Create Student API",
+            error
+        })
+    }
 }
 
-module.exports = { getAllStudentController }
+module.exports = { getAllStudentController, getStudentByIdController, createStudentController }
--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { getAllStudentController } = require('../controller/studentController');
+const { getAllStudentController, getStudentByIdController, createStudentController } = require('../controller/studentController');
 
 const router = express.Router();
 
 // Route
 
 // Get all Student list || GET
-router.get('/getall', getAllStudentController)
+router.get('/getall', getAllStudentController);
+
+// Get Student by ID || GET
+router.get('/get/:id', getStudentByIdController);
+
+// Create Student || POST
+router.post('/create', createStudentController);
 
 module.exports = router;
